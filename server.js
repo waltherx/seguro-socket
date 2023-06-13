580a92b267519c38a1e985d5013bf12c0f74a641
waltherx <waltermj2012@gmail.com>
2023-06-13 15:24:51 -0400

    bug favicon

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello world</h1>");
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.get("/home", (req, res) => {
