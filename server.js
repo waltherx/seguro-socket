3d4813d94d215364c6a4aed1caac527d7367677c
waltherx <waltermj2012@gmail.com>
2023-06-14 16:39:56 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-app.use(cors());
+//app.use(cors());
+app.use(cors({ origin: true }));
 const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 const port = process.env.PORT || 3000;
-
-
-
 
 server.listen(port, () => {
   console.log(`server corriendo *:${port}`);
