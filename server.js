ec7329de781fdce2d0120f4552e5f9ae8bc5437c
waltherx <waltermj2012@gmail.com>
2023-06-14 15:45:43 -0400

    bugs

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 const port = process.env.PORT || 3000;
-const cors = require('cors')
+const cors = require("cors");
 
 app.use(cors());
 
@@ -17,9 +17,10 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-io.on("connection", (socket) => {  
-  socket.on("ubicacion", (data) => {
-    console.log(socket.id,JSON.stringify(data));
-    io.emit('ubicacion', data);
+io.on("connection", (socket) => {
+
+  socket.on("ubicacion", (data) => {    
+    console.log(socket.id, JSON.stringify(data));
+    io.emit("ubicacion", data);
   });
 });
