c5bd0392c3d4ba950fb4fcb0ae8c66bc5402b5a7
waltherx <waltermj2012@gmail.com>
2023-06-14 20:51:49 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
+
 const io = new Server(server,{
   cors: {
     origin: "*"
